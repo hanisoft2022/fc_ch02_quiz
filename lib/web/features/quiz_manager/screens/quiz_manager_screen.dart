4eca34bafe00b4ccf90f83849efe5761aafbc43c
hani <hanisoft2022@gmail.com>
2025-06-12 20:31:12 +0900

    quiz_manager_screen config

--- lib/web/features/quiz_manager/screens/quiz_manager_screen.dart
+++ lib/web/features/quiz_manager/screens/quiz_manager_screen.dart
@@ -1,28 +1,37 @@
+import 'package:ch02_realtime_quiz/common/model/quiz.dart';
 import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class QuizManagerScreen extends StatelessWidget {
   const QuizManagerScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    List<QuizManager> quizManagers = [
+      QuizManager(problems: [], title: 'quiz1'),
+      QuizManager(problems: [], title: 'quiz2'),
+    ];
+
     return DefaultTabController(
-      length: 3,
+      length: 2,
       child: Scaffold(
         appBar: AppBar(
-          title: Text('Tab 예제'),
+          title: Text('퀴즈 출제하기(출제자용)'),
           bottom: TabBar(
             tabs: [
-              Tab(icon: Icon(Icons.home), text: '홈'),
-              Tab(icon: Icon(Icons.star), text: '즐겨찾기'),
-              Tab(icon: Icon(Icons.settings), text: '설정'),
+              Tab(icon: Icon(Icons.home), text: '출제하기'),
+              Tab(icon: Icon(Icons.star), text: '퀴즈 목록'),
             ],
           ),
         ),
         body: TabBarView(
           children: [
-            Center(child: Text('홈 페이지')),
-            Center(child: Text('즐겨찾기 페이지')),
-            Center(child: Text('설정 페이지')),
+            ListView.builder(
+              itemCount: quizManagers.length,
+              itemBuilder: (context, index) =>
+                  ExpansionTile(title: quizManagers[index].title.text.make()),
+            ),
+            Center(child: Text('퀴즈 목록')),
           ],
         ),
       ),
